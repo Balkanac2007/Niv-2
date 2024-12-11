ac7f133b4de355dd4f9139f16df22175a5eed62a
arsu07001 <arsu07001@E-PW05CGQW>
2024-12-11 13:23:38 +0100

    Add project files.

---
+++ Nivå2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int tal;
+
+        do
+        {
+            Console.Write("Skriv in ett heltal (fortsätt skriva tills du skrivit 0): ");
+            tal = int.Parse(Console.ReadLine());
+        } while (tal != 0);
+
+        Console.WriteLine("Du skrev in 0. Programmet avslutas.");
+    }
+}
+
